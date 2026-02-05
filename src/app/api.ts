4d0522fdf6fb211dcbef3762f2ba78c220cb61c3
LiudmylaTymoshenko <liuda.tymoshenko@gmail.com>
2026-02-04 21:22:44 -0500

    fix url

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,10 +1,12 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import type { RootState } from './store';
 
+const apiUrl = import.meta.env.VITE_API_URL;
+
 export const api = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://lore-builder-backend.onrender.com',
+    baseUrl: apiUrl,
     prepareHeaders: (headers, { getState }) => {
       const token = (getState() as RootState).auth.accessToken;
 
